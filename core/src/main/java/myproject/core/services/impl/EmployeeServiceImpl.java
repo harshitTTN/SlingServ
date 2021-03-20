5ec8251caa92c337847c27a2f212e4a7f08e5942
Harshit TTN <harshit.abrol@tothenew.com>
2021-03-20 16:41:29 +0530

    Runmodes full

--- core/src/main/java/myproject/core/services/impl/EmployeeServiceImpl.java
+++ core/src/main/java/myproject/core/services/impl/EmployeeServiceImpl.java
@@ -2,9 +2,12 @@ package myproject.core.services.impl;
 
 
 import myproject.core.services.EmployeeService;
+import myproject.core.services.EmployeeServiceConfig;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.Designate;
 
 @Component(service = EmployeeService.class)
+@Designate(ocd = EmployeeServiceConfig.class)
 public class EmployeeServiceImpl implements EmployeeService {
     private String name;
     private int id;
