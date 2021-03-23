0c83324a767174f9ee60deeaa895faeb4b1a79ee
Harshit TTN <harshit.abrol@tothenew.com>
2021-03-23 14:24:33 +0530

    Q3

---
+++ ui.apps/src/main/content/jcr_root/apps/myproject/components/page/DropDownList1.java
@@ -0,0 +1,62 @@
+package apps.myproject.components.page;
+
+
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import org.apache.sling.api.resource.Resource;
+
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.script.Bindings;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class DropDownList1 extends WCMUsePojo{
+    private Map<String,String> dropdownMap=new HashMap<>();
+    public static final String PATH="/content/my-options";
+    @Override
+    public void activate() throws Exception {
+
+        dropdownMap= getData();
+
+    }
+
+    private Map<String,String> getData(){
+        Map<String,String> map =new HashMap<>();
+
+        Resource pageResource=getResourceResolver().getResource(PATH);
+        if(pageResource !=null)
+        {
+            Iterable<Resource> resourceChildrens=  pageResource.getChildren();
+            String text="";
+
+            String color="";
+            for(Resource children : resourceChildrens)
+            {
+                Node node=children.adaptTo(Node.class);
+                try {
+                    text=node.getProperty("text").getValue().toString();
+
+                    color=node.getProperty("color").getValue().toString();
+                } catch (RepositoryException e) {
+                    e.printStackTrace();
+                }
+
+                map.put(color,text);
+
+            }
+
+        }
+        return map;
+    }
+
+    public Map<String, String> getDropdownMap() {
+        return dropdownMap;
+    }
+}
+
+
